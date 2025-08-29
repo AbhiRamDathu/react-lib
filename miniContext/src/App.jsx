896351fcc429948428fbebffa58e5303914d038f
Abhi Ram Dathu <lingarajuvelishala11@gmail.com>
2025-08-29 05:42:59 +0530

    Context api  discussion

---
+++ miniContext/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Login from "./Components/Login"
+import Profile from "./Components/Profile"
+import UserContextProvider from "./Context/userContextProvider"
+
+function App() {
+
+
+  return (
+    <UserContextProvider>
+    <h1>React with Abhi Ram Dathu</h1>
+    <Login />
+    <Profile />
+    </UserContextProvider>
+  )
+}
+
+export default App
