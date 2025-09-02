76ec4b5f8ec6238dc2a0a2e6ee5c14d10d72a648
Abhi Ram Dathu <lingarajuvelishala11@gmail.com>
2025-09-02 05:46:10 +0530

    TodoContext-com

---
+++ todoContext/src/Components/TodoItem.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import { useTodo } from "../Contexts/TodoContexts";
+
+function TodoItem({ todo }) {
+    const [isTodoEditable, setIsTodoEditable] = useState(false)
+    const [todoMsg, setTodoMsg] = useState(todo.todo)
+    const { updateTodo, deleteTodo, toggleComplete } = useTodo()
+
+    const editTodo = () => {
+        if(todoMsg.trim()) {
+        updateTodo(todo.id, {...todo, todo: todoMsg.trim()})
+        setIsTodoEditable(false);
+       }
+    };
+    const toggleCompleted = () => {
+        toggleComplete(todo.id)
+    }
+
+
+    return(
+        <div className={`flex border border-black/10 rounded-lg px-3 py-3 gap-x-3 shadow-sm shadow-white/50 duration-300 text-black ${
+            todo.completed ? "bg-[#c6e9a7]": "bg-[#ccbed7]"}`}>
+            <input 
+            type="text"
+            className="cursor-pointer w-4 h-4 accent-green-600"
+            checked={todo.completed}
+            onChange={toggleCompleted}
+            />
+            <input 
+            type="text"
+            className={`border outline-none w-full bg-transparent rounded-lg ${
+                isTodoEditable ? "border-black/10 px-2" :
+                "border-transparent bg-transparent"
+            } ${todo.completed ? "Line-through text-gray-500" : "text-gray-800"}`} 
+            value={todoMsg}
+            onChange={(e) => setTodoMsg(e.target.value)}
+            readOnly={!isTodoEditable}
+             />
+            {/* Edit, Save Button */}
+            <button 
+            className="inline-flex w-8 h-8 rounded-lg text-lg border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0 transition-all disabled:opacity-50"
+            onClick={() => {
+                if(todo.completed) return;
+
+                if(isTodoEditable) {
+                    editTodo();
+                }else setIsTodoEditable ((prev) => !prev);
+            }}
+            disabled={todo.completed}
+            >
+                {isTodoEditable ? "📂" : "✏"}
+            </button>
+            {/* Delete Todo Button */}
+            <button  
+            className="inline-flex w-8 h-8 rouded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0"
+            onClick={(e) => deleteTodo(todo.id)}
+            >
+                ❌
+            </button>
+        </div>
+    );
+}
+
+export default TodoItem
